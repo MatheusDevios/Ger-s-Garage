b98bad513d6aaf50bdf6bbf1ca3286c801a0568f
Matheus Diniz <matheusdiniz@Matheuss-MacBook-Pro.local>
2023-04-01 16:13:57 +0100

    build sent to prod

---
+++ client/build/static/js/664.23405948.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgers_garage=self.webpackChunkgers_garage||[]).push([[664],{1664:function(e,a,r){r.r(a);var n=r(8859),s=r(7689),i=r(184);a.default=function(){var e=(0,s.TH)().state.from;return(0,i.jsx)("div",{children:(0,i.jsx)(n.Z,{invoice:e,adminPage:!1})})}}}]);
+//# sourceMappingURL=664.23405948.chunk.js.map
